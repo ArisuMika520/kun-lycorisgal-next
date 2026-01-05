1479a5d621d091405f281d3cb08129d0b0db5938
ArisuMika <1907653220@qq.com>
2026-01-05 18:43:40 +0800

    feat: 添加更多移动导航项

--- constants/top-bar.ts
+++ constants/top-bar.ts
@@ -31,5 +31,25 @@ export const kunMobileNavItem: KunNavItem[] = [
   {
     name: '加入我们',
     href: 'https://t.me/LyCoriseGAL'
+  },
+  {
+    name: '以下为广告捏~',
+    href: '/'
+  },
+  {
+    name: 'Ai女友💋（在线游玩）',
+    href: 'https://dearestie.xyz?ref_id=88f10d5a-aa3a-47a1-b850-94927bf7ba2f'
+  },
+  {
+    name: '⚡️翻墙Vpn推荐',
+    href: 'https://eueua.cc/#/register?code=u9ev6t6U'
+  },
+  {
+    name: '木瓜玩-精品成人手遊聚合平台',
+    href: 'https://t.glgnd.com/?pid=77'
+  },
+  {
+    name: 'SoulAI',
+    href: 'https://sch.agz1g.com/?channel=7018'
   }
 ]
